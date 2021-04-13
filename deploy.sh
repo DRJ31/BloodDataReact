958337e97bea1beb612e19ed3f94a5440c98909c
dengrenjie <dengrenjie31@gmail.com>
2021-04-13 09:44:44 +0800

    Fix wrong deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar xvf public.tar.xz
+tar xvf build.tar.xz
 sudo rm -rf /var/www/blood
-sudo mv public /var/www/blood
+sudo mv build /var/www/blood
 sudo chown -R www-data:www-data /var/www/blood
