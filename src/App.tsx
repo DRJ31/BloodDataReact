15f81e122565544723a6eb344e6f2e4fc4c4f5ca
dengrenjie <dengrenjie31@gmail.com>
2021-04-14 23:18:47 +0800

    Add link to edit page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import {
-    BrowserRouter as Router,
+    useLocation,
     Switch,
     Route,
     Link,
 } from 'react-router-dom';
-import { Layout, Typography, Result, Button } from 'antd';
+import { Layout, Typography, Result, Button, Menu, Dropdown, message } from 'antd';
+import { UserOutlined, EditOutlined, LogoutOutlined } from '@ant-design/icons';
+import axios from 'axios';
 import './App.css';
 import LoginPage from './pages/Login';
 import TablePage from './pages/Table';
@@ -13,37 +15,76 @@ import FormPage from './pages/Form';
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
+const whitelist = ["/", "/input"];
 
-const App: FC = () => (
-  <div className="App">
-      <Header style={{ textAlign: "left" }}>
-          <Title style={{ color: "white", lineHeight: "2.7em" }} level={3}>
-            <a href="/" style={{ color: "white" }}>Blood Data</a>
-          </Title>
-      </Header>
-      <Content style={{ padding: "2em" }}>
-          <Router>
-            <Switch>
-                <Route path="/login">
-                    <LoginPage />
-                </Route>
-                <Route exact path="/">
-                    <TablePage />
-                </Route>
-                <Route path="/input">
-                    <FormPage />
-                </Route>
-                <Route path="*">
-                    <Result
-                        status="404"
-                        title="404 Not Found"
-                        subTitle="Sorry, the page you visited does not exist."
-                        extra={<Link to="/"><Button type="primary">Back Home</Button></Link>}
-                    />
-                </Route>
-            </Switch>
-          </Router>
-      </Content>
-  </div>
-);
+const App: FC = () => {
+    const { pathname } = useLocation();
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        setShow(whitelist.includes(window.location.pathname));
+    }, [pathname])
+    
+    const logout = () => {
+        const username: string | null = window.localStorage.getItem("username");
+        if (username) {
+            axios.post("/api/logout", { username })
+            .then(response => {
+                message.success(response.data.message);
+                window.location.href = "/login";
+            })
+            .catch(err => {
+                if (err.response && err.response.data.message) {
+                    message.error(err.response.data.message);
+                }
+                window.location.href = "/login";
+            })
+        }
+    }
+    
+    const menu = (
+        <Menu>
+          <Menu.Item key="edit" icon={<EditOutlined />}>
+            <Link to="/input">编辑</Link>
+          </Menu.Item>
+          <Menu.Item key="logout" icon={<LogoutOutlined />}>
+            <span onClick={logout}>退出</span>
+          </Menu.Item>
+        </Menu>
+    );
+
+    return (
+        <div className="app">
+            <Header>
+                <Title style={{ lineHeight: "2.7em", float: "left" }} level={3}>
+                  <Link to="/" style={{ color: "white" }}>Blood Data</Link>
+                </Title>
+                {show && <Dropdown overlay={menu} trigger={['click', 'hover']}>
+                    <UserOutlined style={{ paddingTop: "1em", fontSize: "1.5em", color: "white", float: "right" }} />
+                </Dropdown>}
+            </Header>
+            <Content style={{ padding: "2em", textAlign: "center" }}>
+              <Switch>
+                  <Route exact path="/login">
+                      <LoginPage />
+                  </Route>
+                  <Route exact path="/">
+                      <TablePage />
+                  </Route>
+                  <Route exact path="/input">
+                      <FormPage />
+                  </Route>
+                  <Route path="*">
+                      <Result
+                          status="404"
+                          title="404 Not Found"
+                          subTitle="Sorry, the page you visited does not exist."
+                          extra={<Link to="/"><Button type="primary">Back Home</Button></Link>}
+                      />
+                  </Route>
+              </Switch>
+            </Content>
+        </div>
+      );
+}
 export default App;
