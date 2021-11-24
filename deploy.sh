c6f3ac4ac7bbe5dff408b0043250616f45d57f6a
dengrenjie <dengrenjie31@gmail.com>
2021-11-24 22:19:18 +0800

    Change deploy settings

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tar xvf build.tar.xz
-sudo rm -rf /var/www/blood
+echo $PASSWORD | sudo -S rm -rf /var/www/blood
 sudo mv build /var/www/blood
 sudo chown -R www-data:www-data /var/www/blood
