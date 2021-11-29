9d56e3b66e1b0ac9225c0315d9108afca60967df
Renjie Deng <dengrenjie31@gmail.com>
2021-11-29 22:35:33 +0800

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tar xvf build.tar.xz
-echo $PASSWORD | sudo -S rm -rf /var/www/blood
+sudo rm -rf /var/www/blood
 sudo mv build /var/www/blood
 sudo chown -R www-data:www-data /var/www/blood
